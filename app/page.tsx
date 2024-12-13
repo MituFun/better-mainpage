ce2b7055893302162a789d21ad0a832ee9accc97
MituFun <102889398+MituFun@users.noreply.github.com>
2024-12-13 20:24:20 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { HomePage } from "@/components/main";
+import { Homepage } from "@/components/main";
 
-export default function HomePage() {
-    return <HomePage />;
+export default function Home() {
+    return <Homepage />;
 }
